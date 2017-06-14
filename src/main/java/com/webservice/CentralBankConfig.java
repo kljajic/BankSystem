313d80dc06ce647d8f8992486cc1fa6a941dcf9e
mirkomikac <mmikac27@gmail.com>
2017-06-15 00:52:52 +0200

    Created basic RTGS client for further implementation and realization.

---
+++ src/main/java/com/webservice/CentralBankConfig.java
@@ -0,0 +1,29 @@
+package com.webservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+//import com.centralbank.wsdl.RtgsRequest;
+//import com.centralbank.wsdl.RtgsResponseOriginator;
+import com.webservice.client.CentralBankClient;
+
+@Configuration
+public class CentralBankConfig {
+	
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("com.bank.wsdl");
+		return marshaller;
+	}
+
+	@Bean
+	public CentralBankClient centralBankClient(Jaxb2Marshaller marshaller) {
+		CentralBankClient client = new CentralBankClient();
+		client.setMarshaller(marshaller);
+		client.setUnmarshaller(marshaller);
+		return client;
+	}
+
+}
---
+++ src/main/java/com/webservice/client/CentralBankClient.java
@@ -0,0 +1,37 @@
+package com.webservice.client;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+
+import javax.xml.transform.stream.StreamResult;
+
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import com.bank.wsdl.Mt103Request;
+import com.bank.wsdl.Mt900Response;
+
+
+public class CentralBankClient extends WebServiceGatewaySupport{
+		
+	public Mt900Response getRtgsResponse(Mt103Request request) throws XmlMappingException, IOException {
+		
+		request.setAmount(new BigDecimal(4000));
+		request.setChargeDialNumber("400");
+		request.setMessageId("messageId");
+		
+		final StringWriter out = new StringWriter();
+		getWebServiceTemplate().getMarshaller().marshal(request, new StreamResult(out));
+		String xml = out.toString();
+		
+		System.out.println(xml);
+		
+		Mt900Response response = (Mt900Response) getWebServiceTemplate()
+				.marshalSendAndReceive("http://localhost:8090/ws/centralBankRtgsRequestService",
+						request, new SoapActionCallback("http://com/xsdSchemas/mt900Response"));
+		System.out.println(response);
+		return response;
+	}
+}
